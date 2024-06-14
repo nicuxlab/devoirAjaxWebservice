ad59fc50cfd6003e3a1a3a5674fb0980b5ec3f14
nicuxlab <sedjroayimande@gmail.com>
2024-06-14 22:11:54 +0200

    Exportation et ajout de la base de donnée au projet

--- championnat.sql
+++ championnat.sql
@@ -1,19 +1,119 @@
-CREATE DATABASE championnat;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 14 juin 2024 à 22:07
+-- Version du serveur : 10.4.20-MariaDB
+-- Version de PHP : 8.0.8
 
-USE championnat;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE Teams (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
 
-CREATE TABLE Matches (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    team1_id INT,
-    team2_id INT,
-    score_team1 INT,
-    score_team2 INT,
-    match_date DATETIME,
-    FOREIGN KEY (team1_id) REFERENCES Teams(id),
-    FOREIGN KEY (team2_id) REFERENCES Teams(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `championnat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matches`
+--
+
+CREATE TABLE `matches` (
+  `id` int(11) NOT NULL,
+  `team1_id` int(11) DEFAULT NULL,
+  `team2_id` int(11) DEFAULT NULL,
+  `score_team1` int(11) DEFAULT NULL,
+  `score_team2` int(11) DEFAULT NULL,
+  `match_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `matches`
+--
+
+INSERT INTO `matches` (`id`, `team1_id`, `team2_id`, `score_team1`, `score_team2`, `match_date`) VALUES
+(1, 1, 2, 3, 2, '2023-06-10 15:00:00'),
+(2, 1, 3, 1, 1, '2023-06-11 17:00:00'),
+(3, 2, 4, 0, 2, '2023-06-12 16:00:00'),
+(4, 3, 4, 2, 3, '2023-06-13 18:00:00'),
+(5, 1, 4, 2, 2, '2023-06-14 15:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `teams`
+--
+
+CREATE TABLE `teams` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `teams`
+--
+
+INSERT INTO `teams` (`id`, `name`) VALUES
+(1, 'Dragon FC'),
+(2, 'Barca FC'),
+(3, 'MIKPON FC'),
+(4, 'ORTB FC');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `team1_id` (`team1_id`),
+  ADD KEY `team2_id` (`team2_id`);
+
+--
+-- Index pour la table `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `matches`
+--
+ALTER TABLE `matches`
+  ADD CONSTRAINT `matches_ibfk_1` FOREIGN KEY (`team1_id`) REFERENCES `teams` (`id`),
+  ADD CONSTRAINT `matches_ibfk_2` FOREIGN KEY (`team2_id`) REFERENCES `teams` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
